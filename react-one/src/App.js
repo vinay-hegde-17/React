33659ae05f0ab7ab55ef9c5ab8a6c28a5c843a35
VinuHegde17 <95705229+VinuHegde17@users.noreply.github.com>
2024-06-03 17:39:29 +0530

    Returning list of values using map function

--- react-one/src/App.js
+++ react-one/src/App.js
@@ -1,16 +1,41 @@
 import './App.css';
 
-function getGreeet(greet) {
-  return greet;
-}
-
+const list = [
+  {
+  title: 'React',
+  url: 'https://reactjs.org/',
+  author: 'Jordan Walke',
+  num_comments: 3,
+  points: 4,
+  objectID: 0,
+  },
+  {
+  title: 'Redux',
+  url: 'https://redux.js.org/',
+  author: 'Dan Abramov, Andrew Clark',
+  num_comments: 2,
+  points: 5,
+  objectID: 1,
+  },
+  ];
 function App() {
 
   return (
     <div className="App">
-      <h1>Hello {getGreeet('Welcome')}</h1>
+      <h1>My hacker story</h1>
       <label htmlFor='search'>Search:</label>
       <input id='search' type='text'></input>
+      <ul>
+        {
+          list.map(function (item) {
+            return <ul key={item.objectID}>
+              {item.title}
+              <br></br>
+              {item.author}
+            </ul>
+          })
+        }
+      </ul>
     </div>
   );
 }
