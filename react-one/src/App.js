d00a1fe8c4f078ae5349b90c151d3bc1915a764f
VinuHegde17 <95705229+VinuHegde17@users.noreply.github.com>
2024-06-03 16:35:56 +0530

    Focusing the label

--- react-one/src/App.js
+++ react-one/src/App.js
@@ -4,8 +4,9 @@ function App() {
   const title='React'
   return (
     <div className="App">
-      <h1>Hello world</h1>
-      {title}
+      <h1>Hello {title}</h1>
+      <label htmlFor='search'>Search:</label>
+      <input id='search' type='text'></input>
     </div>
   );
 }
