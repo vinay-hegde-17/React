dfa25a81ab9a8b07f0fb80378315f7b61386d0b0
VinuHegde17 <95705229+VinuHegde17@users.noreply.github.com>
2024-06-03 16:46:46 +0530

    JSX with an objects

--- react-one/src/App.js
+++ react-one/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 
 function App() {
-  const title='React'
+  const welcome={
+    greeting:"Welcome! ",
+    title:"React"
+  }
   return (
     <div className="App">
-      <h1>Hello {title}</h1>
+      <h1>Hello {welcome.greeting} {welcome.title}</h1>
       <label htmlFor='search'>Search:</label>
       <input id='search' type='text'></input>
     </div>
