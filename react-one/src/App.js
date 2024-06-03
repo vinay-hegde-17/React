25f8c4fe53c8e39624cb84fd5b279cc2936c8bb5
VinuHegde17 <95705229+VinuHegde17@users.noreply.github.com>
2024-06-03 22:40:58 +0530

    Props using map

--- react-one/src/App.js
+++ react-one/src/App.js
@@ -1,47 +1,45 @@
-import './App.css';
+import "./App.css";
 
 const list = [
   {
-  title: 'React',
-  url: 'https://reactjs.org/',
-  author: 'Jordan Walke',
-  num_comments: 3,
-  points: 4,
-  objectID: 0,
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
   },
   {
-  title: 'Redux',
-  url: 'https://redux.js.org/',
-  author: 'Dan Abramov, Andrew Clark',
-  num_comments: 2,
-  points: 5,
-  objectID: 1,
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
   },
-  ];
+];
 
-const numbers=[1,2,3,4,5]
-const res=numbers.map(function (num) {
-  return num * num
-})
+const List = (props) => {
+  return (
+    <ul>
+      {props.list.map((item) => (
+        <li key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
 function App() {
-
   return (
     <div className="App">
-      <h1>My hacker story</h1>
-      <label htmlFor='search'>Search:</label>
-      <input id='search' type='text'></input>
-      <ul>
-        {
-          list.map(function (item) {
-            return <ul key={item.objectID}>
-              {item.title}
-              <br></br>
-              {item.author}
-            </ul>
-          })
-        }
-      </ul>
-      <h1>{''+res}</h1>
+      <List list={list} />
     </div>
   );
 }
