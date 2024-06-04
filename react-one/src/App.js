63d6ca0ae3eb6f9de161d15b2a40cc240cdde01f
VinuHegde17 <95705229+VinuHegde17@users.noreply.github.com>
2024-06-04 10:12:13 +0530

    Event handle

--- react-one/src/App.js
+++ react-one/src/App.js
@@ -1,45 +1,16 @@
 import "./App.css";
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
 
-const List = (props) => {
-  return (
-    <ul>
-      {props.list.map((item) => (
-        <li key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-        </li>
-      ))}
-    </ul>
-  );
-};
+ const handleFunction=(event)=>{
+  console.log(event);
+  console.log(event.target.value);
+ }
 
 function App() {
   return (
     <div className="App">
-      <List list={list} />
+      <label htmlFor="search">Search:</label>
+      <input id="search" type="text" onChange={handleFunction}></input>
     </div>
   );
 }
