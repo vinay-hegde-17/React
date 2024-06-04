1d1cb49af4108e45a7320271a590e643b49ada92
VinuHegde17 <95705229+VinuHegde17@users.noreply.github.com>
2024-06-04 15:00:53 +0530

    Life cycle example

--- react-one/src/App.js
+++ react-one/src/App.js
@@ -1,47 +1,33 @@
-import Another from "./Another";
+import { useState } from "react";
+import ParentComponent from "./Another";
 import "./App.css";
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-const List = (props) => {
+const Search = () => {
+  // let searchTerm = '';
+  // const handleChange = (event) => {
+  // searchTerm = event.target.value;
+  // };
+  const[searchTerm ,setsearchTerm]=useState('')
+  
+   const handlefunction =(event)=> {
+    setsearchTerm(event.target.value)
+  }
   return (
-    <ul>
-      {props.list.map((item) => (
-        <li key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-        </li>
-      ))}
-    </ul>
+  <div>
+  <label htmlFor="search">Search: </label>
+  <input id="search" type="text" onChange={handlefunction} />
+  <p>
+  Searching for <strong>{searchTerm}</strong>.
+  </p>
+  </div>
   );
-};
-
+  };
+  
 function App() {
   return (
     <div className="App">
-      <List list={list} />
-      <Another list={list}/>
+    <Search/>
+    <ParentComponent/>
     </div>
   );
 }
