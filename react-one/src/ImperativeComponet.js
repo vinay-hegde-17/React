71dee4d0c4b06c6fefce892a850c5744cbb5899d
VinuHegde17 <95705229+VinuHegde17@users.noreply.github.com>
2024-06-11 21:56:09 +0530

    Imperative

--- react-one/src/ImperativeComponet.js
+++ react-one/src/ImperativeComponet.js
@@ -5,15 +5,17 @@ function ImperativeComponent() {
 
   const handleClick = () => {
     const newDiv = document.createElement("div");
+    newDiv.id=Math.random();
     newDiv.textContent = "New Content";
     divRef.current.appendChild(newDiv);
     console.log(newDiv);
+    console.log(Math.random);
   };
 
   return (
-    <div>
-      <div ref={divRef}>Existing Content</div>
-      <button onClick={handleClick}>Add New Content Imperatively</button>
+    <div >
+      <div id="myfirstdiv" ref={divRef} style={{border:'1px solid red'}}>Existing Content</div>
+      <button id="myfirstbutton" onClick={handleClick} style={{border:'1px solid red'}}>Add New Content Imperatively</button>
     </div>
   );
 }
