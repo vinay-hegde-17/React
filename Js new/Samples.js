c92f42e4dad1ae5b4b5e2337b8cdd695803cfce2
VinuHegde17 <95705229+VinuHegde17@users.noreply.github.com>
2024-06-11 21:51:52 +0530

    JS basics

---
+++ Js new/Samples.js
@@ -0,0 +1,74 @@
+// // function show() {
+// //   console.log(this);
+// // }
+// // show();
+
+// // const dataset = {
+// //   lang: "Java Script",
+// //   itstype: "React",
+// //   display: function () {
+// //     return this;
+// //   },
+// // };
+// // console.log(dataset.display());
+
+// // function disp() {
+// //   return this;
+// // }
+// // console.log(disp());
+
+// var a = 5;
+// console.log(a);
+// var a = 10;
+// console.log(a);
+// let b = 5;
+// console.log(b);
+// b = 6;
+// console.log(b);
+// const c = 8;
+// console.log(c);
+// // c = 8;
+// // console.log(c);
+// function showVal() {
+//    console.log("Inside function a " + a);
+//    console.log("Inside function b " + b);
+//    console.log("Inside function c " + c);
+//    var x=1
+//    let y=2
+//    const z=3
+// }
+// showVal()
+
+// console.log("Outside function x " + x);
+// // console.log("Outside function y " + y);
+// // console.log("Outside function z " + z);
+
+// {
+//     console.log("Inside scope a " + a);
+//     console.log("Inside scope b " + b);
+//     console.log("Inside scope c " + c);
+//     var x=1
+//     let y=2
+//     const z=3
+// }
+// console.log("Outside scope x " + x);
+// // console.log("Outside scope y " + y);
+// // console.log("Outside scope z " + z);
+
+// const person = {
+//   test() {
+//     // console.log('test this');
+//     // console.log(this);
+//     setTimeout(() => {
+//       console.log("arrow this");
+//       console.log(this);
+//     }, 1000);
+//     setTimeout(function () {
+//       console.log("function this");
+//       console.log(this);
+//     }, 1000);
+//   },
+// };
+// person.test();
+
+
